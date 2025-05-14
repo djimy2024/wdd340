919b3448bf0b432cea7b95b791ea037f2034e889
Francillon2024 <fra24017@byui.edu>
2025-05-13 22:57:17 -0400

    adding

--- server.js
+++ server.js
@@ -1,30 +1,40 @@
 /* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
+ * This is the application server
+ * ******************************************/
 const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
 
-/* ***********************
- * Routes
- *************************/
+const app = express()
+
+const expressLayouts = require("express-ejs-layouts")
+
+/* ******************************************
+ * Default GET route
+ * ***************************************** */
+app.get("/", (req, res) => {res.send("Welcome home!")})
+
+
+// index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+
+/*View Engine and Templates*/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+const static = require('./routes/static')
 app.use(static)
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+/* ******************************************
+ * Server host name and port
+ * ***************************************** */
+const HOST = 'localhost'
+const PORT = 3000
 
 /* ***********************
- * Log statement to confirm server operation
- *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+* Log statement to confirm server operation
+* *********************** */
+app.listen(PORT, () => {
+console.log(`trial app listening on ${HOST}:${PORT}`)
 })
